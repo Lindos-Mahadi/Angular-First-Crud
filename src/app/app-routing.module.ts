8eb4c497382eeb7d070198236ca44f9d3457f8bc
Lindos Mahadi <lindosmahadi@gmail.com>
2022-08-25 11:03:35 +0600

    Routing SetUp & Update db.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddRestoComponent } from './header/add-resto/add-resto.component';
+import { UpdateRestoComponent } from './header/update-resto/update-resto.component';
+import { ListRestoComponent } from './header/list-resto/list-resto.component';
+import { RegisterRestoComponent } from './header/register-resto/register-resto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: AddRestoComponent,
+    path: 'add'
+  },
+  {
+    component: ListRestoComponent,
+    path: 'list'
+  },
+  {
+    component: RegisterRestoComponent,
+    path: 'register'
+  },
+  {
+    component: UpdateRestoComponent,
+    path: 'update'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderModule } from './header/header.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +12,7 @@ import { HeaderModule } from './header/header.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
-    HeaderModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
